8e7b2c5b57fdeb1a37e146aceca7c3ed6dd17678
operator.malcolm <andurilan@gmail.com>
2017-07-05 19:35:15 -0400

    Create initial.commit.sh
    Initial Checkout Commit

---
+++ initial.commit.sh
@@ -0,0 +1,15 @@
+# Clone just the repository's .git folder (excluding files as they are already in
+# `existing-dir`) into an empty temporary directory
+git clone --no-checkout hello-mr-justin-lucas /git.tmp # might want --no-hardlinks for cloning local repo
+
+# Move the .git folder to the directory with the files.
+# This makes `existing-dir` a git repo.
+mv /git.tmp/.git /
+
+# Delete the temporary directory
+rmdir /git.tmp
+cd /
+
+# git thinks all files are deleted, this reverts the state of the repo to HEAD.
+# WARNING: any local changes to the files will be lost.
+git reset --hard HEAD
