147cfb477a6e60c0ab9536e924555707dce08ea6
operator.malcolm <andurilan@gmail.com>
2017-07-05 20:32:35 -0400

    Update permit.sh

--- permit.sh
+++ permit.sh
@@ -1,4 +1,4 @@
 chmod +x /opt/initial/initial.commit.sh
 chmod +x /opt/initial/clean.opt.sh
-chmod +x /opt/initial/initial.commit.sh
+chmod +x /opt/initial/directory.remove.sh
 chmod +x /opt/initial/initial.commit.sh
