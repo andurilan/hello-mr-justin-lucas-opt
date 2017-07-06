47e849c3abfd70406e254d3ebe444cb705f69e3e
operator.malcolm <andurilan@gmail.com>
2017-07-05 20:21:00 -0400

    Update initial.commit.sh
    http://www.folkstalk.com/2013/11/check-directory-exists-shell-script.html

--- initial.commit.sh
+++ initial.commit.sh
@@ -1,7 +1,6 @@
 # Clone just the repository's .git folder (excluding files as they are already in
 # `existing-dir`) into an empty temporary directory
-mkdir /git.tmp || true
-rmdir /git.tmp
+directory_remove.sh /git.tmp
 
 git clone --no-checkout https://github.com/andurilan/hello-mr-justin-lucas.git /git.tmp # might want --no-hardlinks for cloning local repo
 
